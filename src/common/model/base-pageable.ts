ba23671df288ca1c5d17876c7e9c83c746a6306d
vngocnd14 <69135114+vngocnd14@users.noreply.github.com>
2021-08-08 00:32:10 +0700

    feat: add compodoc

--- src/common/model/base-pageable.ts
+++ src/common/model/base-pageable.ts
@@ -1,14 +1,6 @@
-interface IProps<T> {
-  items: T[];
-}
-
 export class BasePageable<T> {
   items: T[];
   page: number;
   limit: number;
   total: number;
-
-  constructor(props: IProps<T>) {
-    this.items = props.items;
-  }
 }
