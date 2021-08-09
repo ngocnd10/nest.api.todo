da7545956023c1fb54805c55c988bdbbb9535279
vngocnd14 <69135114+vngocnd14@users.noreply.github.com>
2021-08-09 17:16:32 +0700

    style: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,8 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'sort-destructure-keys'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,31 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': ['error', '1tbs'],
+    curly: ['error', 'all'],
+    'object-shorthand': ['warn', 'always'],
+    'prefer-destructuring': [
+      'warn',
+      {
+        array: true,
+        object: true,
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    'no-duplicate-imports': 'off',
+    '@typescript-eslint/no-duplicate-imports': ['error'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': ['error'],
+    '@typescript-eslint/prefer-optional-chain': ['error'],
+    quotes: 'off',
+    '@typescript-eslint/quotes': ['warn', 'single', { allowTemplateLiterals: true }],
+    'sort-destructure-keys/sort-destructure-keys': [1, { caseSensitive: false }],
+    "prettier/prettier": ["error", { "endOfLine": "auto" }],
+    eqeqeq: "error"
   },
 };
